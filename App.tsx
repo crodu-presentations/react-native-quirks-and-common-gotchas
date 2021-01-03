a06683e44c43dec4d1f331aa0e1e4142669044f4
Joanna Patrzyk <jpatrzyk@users.noreply.github.com>
2021-01-03 13:01:43 +0100

    feat(2-no-css): preparation

--- App.tsx
+++ App.tsx
@@ -2,17 +2,9 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 const App = () => {
-  let myText = 'hello world';
-  myText = '';
-
-  let myValue = 5;
-  myValue = 0;
-
   return (
     <View testID="container" style={styles.container}>
       <Text style={styles.title}>Hello dev.js</Text>
-      {!!myText && <Text style={styles.description}>{myText}</Text>}
-      {!!myValue && <Text style={styles.description}>{myValue}</Text>}
     </View>
   );
 };
@@ -27,12 +19,6 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 24,
     fontWeight: '600',
-    color: 'black',
-  },
-  description: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
     color: '#333',
   },
 });
