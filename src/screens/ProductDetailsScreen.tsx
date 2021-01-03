7e2194c95fa6ce6cb5700398373b40227bc5f39b
Joanna Patrzyk <jpatrzyk@users.noreply.github.com>
2021-01-03 14:18:07 +0100

    feat(5-redux-and-navigation): problem - performance issue in screens connected to redux

--- src/screens/ProductDetailsScreen.tsx
+++ src/screens/ProductDetailsScreen.tsx
@@ -1,22 +1,78 @@
 import { RouteProp } from '@react-navigation/native';
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useCallback } from 'react';
+import { Image, Switch, Text, useWindowDimensions, View } from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
+import { connect, ConnectedProps } from 'react-redux';
 import { ProductStackParams } from '../navigation/navigatorParams';
 import { Routes } from '../navigation/Routes';
+import { toggleWishlist } from '../redux/products/productActions';
+import { RootState } from '../redux/rootReducer';
+import { Typography } from '../styles/fonts';
 
 type CurrentRouteProp = RouteProp<ProductStackParams, Routes.ProductDetails>;
 
-interface Props {
+interface OwnProps {
   route: CurrentRouteProp;
 }
 
-export const ProductDetailsScreen = ({ route }: Props) => {
-  const productId = route.params?.productId;
+const mapState = (state: RootState, ownProps: OwnProps) => ({
+  product: state.products.products.find(
+    (p) => p.id === ownProps.route.params.productId,
+  ),
+});
+
+const mapDispatch = {
+  onToggleWishlist: toggleWishlist,
+};
+
+const connector = connect(mapState, mapDispatch);
+
+type PropsFromRedux = ConnectedProps<typeof connector>;
+
+type Props = PropsFromRedux & OwnProps;
+
+const ProductDetailsScreen = ({ product, onToggleWishlist }: Props) => {
+  const { width, height } = useWindowDimensions();
+  const imgHeight = 0.4 * height;
+  const productId = product?.id;
+
+  const handleToggle = useCallback(() => {
+    if (productId) {
+      onToggleWishlist(productId);
+    }
+  }, [productId, onToggleWishlist]);
+
+  if (!product) {
+    return null;
+  }
 
   return (
     <View style={styles.container}>
-      <Text>Product ID: {productId}</Text>
+      <Image
+        source={{ uri: product.imageUrl }}
+        width={width}
+        height={imgHeight}
+        resizeMode="cover"
+        style={[
+          {
+            width,
+            height: imgHeight,
+          },
+          styles.image,
+        ]}
+      />
+      <Text style={[Typography.bodyStrong, styles.name]}>{product.name}</Text>
+      <Text style={[Typography.body, styles.description]}>
+        {product.description}
+      </Text>
+      <View style={styles.wishlistContainer}>
+        <Text style={[Typography.bodyStrong, styles.wishlistLabel]}>
+          {product.isOnWishlist
+            ? 'On Wishlist - toggle to remove'
+            : 'Add to your Wish List'}
+        </Text>
+        <Switch onValueChange={handleToggle} value={product.isOnWishlist} />
+      </View>
     </View>
   );
 };
@@ -24,7 +80,30 @@ export const ProductDetailsScreen = ({ route }: Props) => {
 const styles = EStyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+  },
+  image: {
+    resizeMode: 'cover',
+  },
+  name: {
+    fontSize: '1.2rem',
+    marginHorizontal: 16,
+    marginTop: 12,
+  },
+  description: {
+    fontSize: '0.8rem',
+    marginHorizontal: 16,
+    marginVertical: 12,
+  },
+  wishlistContainer: {
+    marginHorizontal: 16,
+    marginVertical: 12,
+    flexDirection: 'row',
     alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  wishlistLabel: {
+    fontSize: '0.8rem',
   },
 });
+
+export default connector(ProductDetailsScreen);
