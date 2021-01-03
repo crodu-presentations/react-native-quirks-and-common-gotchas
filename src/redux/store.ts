74f32d3ac46677c9e653e7c2d513ee044196adb0
Joanna Patrzyk <jpatrzyk@users.noreply.github.com>
2021-01-03 14:27:46 +0100

    feat(debugging): add react-devtools & integration with redux-devtools-extension

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,22 @@
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, createStore, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import { rootReducer } from './rootReducer';
 import { rootSaga } from './rootSaga';
 
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+const composeEnhancers = (() => {
+  if (__DEV__) {
+    // @ts-ignore: in DEV mode, redux-devtools-extension could be available
+    return window?.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  }
+  return compose;
+})();
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(sagaMiddleware)),
+);
 
 sagaMiddleware.run(rootSaga);
 
