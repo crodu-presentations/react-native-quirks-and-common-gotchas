8efc44c53a0075a369dafda851e6f4f5388a4d1e
Joanna Patrzyk <jpatrzyk@users.noreply.github.com>
2021-01-03 13:47:20 +0100

    fix(3-animations): add jest.useFakeTimers() to test animated components

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -5,6 +5,8 @@ import App from '../src/App';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
+jest.useFakeTimers();
+
 describe('App', () => {
   it('renders correctly', () => {
     renderer.create(<App />);
